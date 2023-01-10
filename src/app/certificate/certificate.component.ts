ed903faf99d776a2cd2280085a8c7c47b9d2982c
Nawaz Pasha <nawaz22022001@gmail.com>
2023-01-10 18:05:48 +0530

    Changes and Certificate Updated

--- src/app/certificate/certificate.component.ts
+++ src/app/certificate/certificate.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { jsPDF } from "jspdf";
+import { font } from './font';
 
 @Component({
   selector: 'app-certificate',
@@ -11,11 +13,38 @@ export class CertificateComponent implements OnInit {
   @ViewChild('namediv', { static: false })
   namediv!: ElementRef;
 
+  @ViewChild('content', { static: false })
+  content!: ElementRef;
+
+  @ViewChild('pname', { static: false })
+  pname!: ElementRef;
+
   constructor(private route: ActivatedRoute, public renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: any) => {
       this.name = params.get('name')
     })
+  }
+
+  public SavePDF(name: string): void {
+    let content = this.content.nativeElement;
+    let doc = new jsPDF('l', 'mm', [150, 211]);
+    let _elementHandlers =
+    {
+      '#editor': function (element: any, renderer: any) {
+        return true;
+      }
+    };
+    this.pname.nativeElement.style.left = '85%'
+    console.log(content.innerHTML);
+    doc.html(content.innerHTML, {
+      callback: function (doc) {
+        doc.save(`${name}`);
+      },
+      width: 135,
+      windowWidth: 500
+    })
+    this.pname.nativeElement.style.left = '55%'
   }
 }
